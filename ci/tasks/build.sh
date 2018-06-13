d821d35b46bbf49be0041607d5fdaa57fab86387
Yaroslav Novytskyy <ynovytskyy@gmail.com>
2018-06-13 14:47:45 -0400

    fix in sh script for the build task

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-
 set -eux
 
-SCRIPT_DIR=$(dirname "$0")
-source ${SCRIPT_DIR}/mvn-tools.sh
-
 export ROOT_FOLDER="$( pwd )"
-
-VERSION=$(cat version/version)
-
 M2_HOME="${HOME}/.m2"
 M2_CACHE="${ROOT_FOLDER}/maven"
-
-[[ -d $M2_CACHE && ! -d $M2_HOME ]] && ln -s $M2_CACHE $M2_HOME
 
 echo "Root folder is [${ROOT_FOLDER}]"
 echo "M2 Home folder is [${M2_HOME}]"
 echo "M2 Cache folder is [${M2_CACHE}]"
-
-echo "Version Number is [${VERSION}]"
 
 pushd source
 
